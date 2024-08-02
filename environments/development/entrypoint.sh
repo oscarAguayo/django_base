2c953189cd5492c4a8edc534e3297931c714bb7c
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-08-01 19:04:06 -0600

    Fix entrypoint.sh for projects that not exist

--- environments/development/entrypoint.sh
+++ environments/development/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Initialize the project if not exist
+if [ ! -f manage.py ]; then
+	echo "Starting the project..."
+	django-admin startproject config .
+	cp -f /compose/$APP_ENV/settings.py config/settings.py
+fi
+
 python manage.py runserver 0.0.0.0:$DJANGO_PORT
 
 exec "$@"
