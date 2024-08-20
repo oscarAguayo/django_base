57d4f8f14e9fdbb902c10cb55555f98ae19d3229
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-08-20 01:56:29 -0600

    Move .dockerignore to root project whe use_environment.sh is executed

--- compose/use_environment.sh
+++ compose/use_environment.sh
@@ -27,6 +27,9 @@ if [[ "$dirs" == *"$1/"* ]]; then
 	# Change to destination path
 	cd "$DEST_DIR"
 
+	# Move .dockerignore file to root project
+	mv "$DIR/base/.dockerignore" "./"
+
 	# Create simbolic link to docker-compose.yml file
 	if [ -f "$DIR/$1/docker-compose.yml" ]; then
 		ln -sf "$DIR/$1/docker-compose.yml" "./docker-compose.yml"
