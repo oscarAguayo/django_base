a1164a68f40276f3b60ccda6a422b16abaf5f8a1
Oscar Aguayo <ooscar.a.c@gmail.com>
2024-09-25 15:20:46 -0600

    Add logging to local and development settings

--- compose/local/settings.py
+++ compose/local/settings.py
@@ -25,6 +25,28 @@ DATABASES = {
     }
 }
 
+##### lOGGING #####
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
+
 ##### THIRD CONFIGURATIONS #####
 
 # Debug Toolbar
